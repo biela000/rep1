104224d66d0f425052edb4769ee1e24fa9e2c9a6
biela000 <jak.biela000@gmail.com>
2021-06-26 16:26:51 +0200

    wyszukiwarka

---
+++ projekty/wyszukiwarka/wyszukiwarka.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+#include<cstdlib>
+
+using String = std::string;
+
+struct Row {
+    String r_Name;
+    double r_X;
+    double r_Y;
+    Row(String string) {
+        string.erase(string.begin(), string.begin() + string.find("PL") + 4);
+        r_Name = string.substr(0, string.find("  "));
+        string.erase(string.begin(), string.begin() + string.find("  ") + 2);
+        r_X = atof(string.substr(0, string.find("  ")).c_str());
+        string.erase(string.begin(), string.begin() + string.find("  ") + 2);
+        r_Y = atof(string.substr(0, string.find("  ")).c_str());
+    }
+};
+
+std::ostream& operator<<(std::ostream& stream, const Row& row) {
+    stream << row.r_Name << " " << row.r_X << " " << row.r_Y;
+    return stream;
+}
+
+int main() {
+    std::fstream f;
+    f.open("miasta.txt", std::ios::in);
+    String s;
+    std::vector<Row> v;
+    while (getline(f, s)) {
+        v.reserve(1);
+        v.emplace_back(s);
+    }
+    for (Row r : v) {
+        std::cout << r << std::endl;
+    }
+    f.close();
+    return 0;
+}
