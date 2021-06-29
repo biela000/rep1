1eebd345551538f031cd5c6383ba07bc5eb9dd46
biela000 <jak.biela000@gmail.com>
2021-06-29 21:57:04 +0200

    std::sort()

---
+++ tutorial/sorting.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<array>
+#include<algorithm>
+#include<functional>
+
+int main() {
+    std::array<int, 5> arr = { 3, 5, 1, 4, 2 };
+    std::sort(arr.begin(), arr.end(), [](const int& a, const int& b){ 
+        if (a == 1) {
+            return false;
+        }
+        if (b == 1) {
+            return true;
+        }
+        return a < b /*jeśli a > b to idzie na początek tablicy*/;
+    }); // jako trzeci argument można tu też użyć "std::greater<int>()" z biblioteki <functional>
+    for (int& value : arr) {
+        std::cout << value << std::endl;
+    }
+    return 0;
+}
