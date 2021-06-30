a228d710b684357a9115c6baf68622929900993d
biela000 <jak.biela000@gmail.com>
2021-06-30 18:45:27 +0200

    precompiled headers

---
+++ tutorial/precompiled_headers.cpp
@@ -0,0 +1,4 @@
+// żeby zrobić precompiled header g++ musimy tylko skompilować header który includujemy w pliku
+// precompiled header zawiera zazwyczaj #include w samym sobie
+// precompiled headers bardzo przyspieszają kompilację naszego kodu
+// precompiled headers powinny zawierać biblioteki których używamy stosunkowo często w naszym projekcie
